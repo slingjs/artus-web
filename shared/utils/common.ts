5172980dc1c26af1e37c79d1b69ad7447e5705b0
王子凌 <sling1994@live.com>
2023-03-29 22:42:03 +0800

    init: init project

---
+++ shared/utils/common.ts
@@ -0,0 +1,12 @@
+export function calcRandomString (len?: number): string {
+  len = len || 32;
+  const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+  const maxPos = $chars.length;
+
+  let pwd = '';
+  for (let i = 0; i < len; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+
+  return pwd;
+}
