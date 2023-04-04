bf7b3ab0703811e19a1155fe832eedf1ea8de9d9
王子凌 <sling1994@live.com>
2023-04-04 16:03:34 +0800

    build(client): config the build base dir

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// import { FILE_BASE_PATH } from '@sling/artus-web-shared/constants/client'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    commonjsOptions: {
+
+    }
+  },
+  base: '/dist'
 })
