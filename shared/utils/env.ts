bf35ebc7bcd6663cd2f79f0fed8fe8d3c92e6732
王子凌 <sling1994@live.com>
2023-04-05 00:21:35 +0800

    chore(shared): enhance the utils

---
+++ shared/utils/env.ts
@@ -0,0 +1,15 @@
+import { compareIgnoreCase } from './string'
+import { DEVELOPMENT_MODE, PRODUCTION_MODE } from '../constants'
+
+export function judgeBuildMode (targetMode: string) {
+  return compareIgnoreCase(process.env.NODE_ENV, targetMode)
+}
+
+export function judgeBuildModeInProduction () {
+  return judgeBuildMode(PRODUCTION_MODE)
+}
+
+
+export function judgeBuildModeInDevelopment () {
+  return judgeBuildMode(DEVELOPMENT_MODE)
+}
--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,3 +1,3 @@
-export * from './common'
+export * from './env'
 export * from './promise'
 export * from './string'
