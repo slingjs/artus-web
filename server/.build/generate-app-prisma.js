5667546c160d66ba017d6ce3438a0833cb309d55
王子凌 <sling1994@live.com>
2023-04-07 02:00:30 +0800

    build(server): enhance the 'postinstall' script

--- server/.build/generate-app-prisma.js
+++ server/.build/generate-app-prisma.js
@@ -3,6 +3,11 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const job = function() {
+  // If not exists
+  if (!fs.existsSync(path.resolve(__dirname, '../app'))) {
+    return
+  }
+
   const schemaPaths = [
     '../app/frameworks/framework-web/models/mongo/schema.prisma',
     '../app/frameworks/framework-web/models/mysql/schema.prisma'
