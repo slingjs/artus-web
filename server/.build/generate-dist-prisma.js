e6591acd31603706fff74aa1d203365d6c9afbbe
王子凌 <sling1994@live.com>
2023-04-06 19:15:49 +0800

    build(server): enhance the generation logic for the dist files

--- server/.build/generate-dist-prisma.js
+++ server/.build/generate-dist-prisma.js
@@ -1,9 +1,10 @@
-const { spawnSync } = require('child_process')
+const { execSync } = require('child_process')
 
-// Generate prisma schemas.
-spawnSync(
-  `
-  npx prisma generate --schema ../dist/app/frameworks/framework-web/models/mongo/schema.prisma &&
-  npx prisma generate --schema ../dist/app/frameworks/framework-web/models/mysql/schema.prisma
-  `.trim()
-)
+const schemaPaths = [
+  '../dist/app/frameworks/framework-web/models/mongo/schema.prisma',
+  '../dist/app/frameworks/framework-web/models/mysql/schema.prisma'
+]
+
+schemaPaths.forEach(p => {
+  execSync('npx prisma generate --schema ' + p, { cwd: __dirname })
+})
