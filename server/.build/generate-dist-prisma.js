07c0319862c0ad60132c04ca0aeed362d013d405
王子凌 <sling1994@live.com>
2023-04-06 21:21:20 +0800

    fix(server): fix the issue that cannot generate dist prisma schemas correctly

--- server/.build/generate-dist-prisma.js
+++ server/.build/generate-dist-prisma.js
@@ -1,10 +1,21 @@
 const { execSync } = require('child_process')
+const fs = require('fs-extra')
+const path = require('path')
 
-const schemaPaths = [
-  '../dist/app/frameworks/framework-web/models/mongo/schema.prisma',
-  '../dist/app/frameworks/framework-web/models/mysql/schema.prisma'
-]
+const job = function() {
+  // If not exists
+  if (!fs.existsSync(path.resolve(__dirname, '../dist'))) {
+    return
+  }
 
-schemaPaths.forEach(p => {
-  execSync('npx prisma generate --schema ' + p, { cwd: __dirname })
-})
+  const schemaPaths = [
+    '../dist/app/frameworks/framework-web/models/mongo/schema.prisma',
+    '../dist/app/frameworks/framework-web/models/mysql/schema.prisma'
+  ]
+
+  schemaPaths.forEach(p => {
+    execSync('npx prisma generate --schema ' + p, { cwd: __dirname })
+  })
+}
+
+job()
