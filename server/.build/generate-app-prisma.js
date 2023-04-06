be629cf8cfd58b5e6dcd935ff46a27f476738702
王子凌 <sling1994@live.com>
2023-04-07 00:40:21 +0800

    feat(server): add a new npm script to generate app prisma schemas

---
+++ server/.build/generate-app-prisma.js
@@ -0,0 +1,16 @@
+const { execSync } = require('child_process')
+const fs = require('fs-extra')
+const path = require('path')
+
+const job = function() {
+  const schemaPaths = [
+    '../app/frameworks/framework-web/models/mongo/schema.prisma',
+    '../app/frameworks/framework-web/models/mysql/schema.prisma'
+  ]
+
+  schemaPaths.forEach(p => {
+    execSync('npx prisma generate --schema ' + p, { cwd: __dirname })
+  })
+}
+
+job()
