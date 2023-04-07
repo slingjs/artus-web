5cedb7924f4bfbb7954f08c75582889034cbc0e6
王子凌 <sling1994@live.com>
2023-04-07 15:21:03 +0800

    docs(shared): enhance the constants' annotation

--- packages/shared/constants/account.ts
+++ packages/shared/constants/account.ts
@@ -1,11 +1,15 @@
 /**
  * Password pattern.
  *
- * At least 8 characters long
- * At least 1 capital letter
- * At least 1 lowercase letter
- * At least 1 special character
- * At least 1 numeric character
+ * gte 6, lte 16 characters.
+ * At least 1 capital letter.
+ * At least 1 lowercase letter.
+ * At least 1 special character.
+ * At least 1 numeric character.
  */
 export const accountPasswordPatternString = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W])[A-Za-z\\d\\W]{6,16}$'
+/**
+ * gte 6, lte 60 characters.
+ * English letters and numbers.
+ */
 export const accountNamePatternString = '^[a-zA-Z\d]{6,60}$'
