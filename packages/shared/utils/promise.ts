efa2a6edcbc38a5c36f234c7802655d1eeecfe82
王子凌 <sling1994@live.com>
2023-04-07 18:44:19 +0800

    chore(shared): add a new method to generate operable promise handler

--- packages/shared/utils/promise.ts
+++ packages/shared/utils/promise.ts
@@ -3,3 +3,20 @@ export function promiseDelay (time: number = 0) {
     setTimeout(resolve, time)
   })
 }
+
+export function generateOperablePromise<T = any> (invoker?: Function) {
+  let resolve: Parameters<ConstructorParameters<typeof Promise<T>>[0]>[0] | undefined = undefined,
+    reject: Parameters<ConstructorParameters<typeof Promise<T>>[0]>[1] | undefined = undefined
+  const p = new Promise<T>(function(_resolve, _reject) {
+    typeof invoker === 'function' && invoker()
+
+    resolve = _resolve
+    reject = _reject
+  })
+
+  return {
+    p,
+    resolve: resolve!,
+    reject: reject!
+  }
+}
