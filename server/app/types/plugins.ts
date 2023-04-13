8bd86b42808c050b87aba6b49c996dabdbdd97a4
王子凌 <sling1994@live.com>
2023-04-13 15:44:09 +0800

    chore(server): add missing variable declaration

--- server/app/types/plugins.ts
+++ server/app/types/plugins.ts
@@ -1,5 +1,7 @@
 import { PluginConfigItem } from '@artus/core'
 
+export const ARTUS_WEB_SHARED_HTTP_SERVER = 'ARTUS_WEB_SHARED_HTTP_SERVER'
+
 export enum PluginsEnum {
   http = 'http',
   redis = 'redis',
