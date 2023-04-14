6bc3c9ff445b91e2740b9cdc85a3a350ed467cdd
王子凌 <sling1994@live.com>
2023-04-14 11:55:12 +0800

    chore(server): try to broadcast the received ws message to each socket which is the same path as the current one

--- server/app/frameworks/framework-web/controllers/ws/account.ts
+++ server/app/frameworks/framework-web/controllers/ws/account.ts
@@ -1,6 +1,11 @@
 import { WebsocketController, WebsocketEvent, WebsocketUse } from '../../../../plugins/plugin-websocket/decorator'
-import { WebSocketEventNames, WebsocketMiddleware } from '../../../../plugins/plugin-websocket/types'
+import {
+  ARTUS_PLUGIN_WEBSOCKET_CLIENT,
+  WebSocketEventNames,
+  WebsocketMiddleware
+} from '../../../../plugins/plugin-websocket/types'
 import { websocketExecutionTimeMiddleware } from '../../middlewares/common/execution-time'
+import { WebsocketClient } from '../../../../plugins/plugin-websocket/client'
 
 @WebsocketController('/ws/account')
 @WebsocketUse([websocketExecutionTimeMiddleware()])
@@ -19,6 +24,24 @@ export default class AccountWsController {
     const [ctx, next] = args
 
     await ctx.input.params.trigger.response(ctx, { receiveMessage: '' })
+
+    await next()
+  }
+
+  @WebsocketEvent(WebSocketEventNames.MESSAGE, { path: '/observe' })
+  async handleMessageAndBroadcast (...args: Parameters<WebsocketMiddleware>) {
+    const [ctx, next] = args
+
+    const { input: { params: { trigger, socket, app, eventArgs } } } = ctx
+
+    const websocketClient = app.container.get(ARTUS_PLUGIN_WEBSOCKET_CLIENT) as WebsocketClient
+
+    const receivedMessage = eventArgs[0];
+    if (receivedMessage) {
+      websocketClient.getWsServerSameReqPathSockets(socket).forEach(s => {
+        trigger.send(s, receivedMessage)
+      })
+    }
 
     await next()
   }
