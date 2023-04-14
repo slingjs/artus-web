5af78c38e59bde0ea147966a924bd90fd446ec85
王子凌 <sling1994@live.com>
2023-04-14 19:02:44 +0800

    types(shared): add some util-types

--- packages/shared/types/utils.ts
+++ packages/shared/types/utils.ts
@@ -11,3 +11,30 @@ export type PropertyType<T, Path extends string> = string extends Path // Not an
 export type MultiPropertyType<K, T extends string> = {
   [P in T]: PropertyType<K, P>;
 }
+
+export type DeepestKeys<T> = T extends string ? never : {
+  [K in keyof T & string]: T[K] extends string ? K : DeepestKeys<T[K]>;
+}[keyof T & string];
+
+export type DeepestPaths<T, Path extends string[] = []> = T extends string ? Path : {
+  [K in keyof T & string]: DeepestPaths<T[K], [...Path, K]>;
+}[keyof T & string];
+
+export type ExcludePath<T, Key extends string, Path extends string[] = []> = T extends string ? Path : {
+  [K in keyof T & string]: K extends Key
+    ? T[K] extends string ? never : ExcludePath<T[K], Key, [...Path, K]>
+    : ExcludePath<T[K], Key, [...Path, K]>;
+}[keyof T & string];
+
+export type PathTo<T, Key extends string> = Exclude<DeepestPaths<T>, ExcludePath<T, Key>> extends [...infer Path, infer _]
+  ? Path
+  : never;
+
+export type GetKeyPaths<T> = { [K in DeepestKeys<T>]: PathTo<T, K>; };
+
+export type NestedKeyOf<ObjectType extends object> =
+  {
+    [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
+    ? `${ Key }` | `${ Key }.${ NestedKeyOf<ObjectType[Key]> }`
+    : `${ Key }`
+  }[keyof ObjectType & (string | number)];
