ecd966f9c7eaca39335096718724e43283fe2ff0
王子凌 <sling1994@live.com>
2023-04-17 14:20:19 +0800

    fix(server): fix the missing of 'lastSignedInAt' property for account user session

--- server/app/frameworks/framework-web/constants/services.ts
+++ server/app/frameworks/framework-web/constants/services.ts
@@ -7,7 +7,7 @@ export const MEMORY_CACHE_DEFAULT_TTL = 5 * 60 * 1000 // ms.
 
 export const USER_DISTRIBUTE_CACHE_DEFAULT_TTL = 30 * 60 * 1000 // ms.
 
-export const ACCESSIBLE_ACCOUNT_PROPERTIES = ['email', 'name', 'roles', 'userId'] as const
+export const ACCESSIBLE_ACCOUNT_PROPERTIES = ['email', 'name', 'roles', 'userId', 'lastSignedInAt'] as const
 export const USER_SESSION_COOKIE_MAX_AGE = 60 * 30 // s.
 export const USER_SESSION_COOKIE_MAX_AGE_REMEMBERED = 60 * 60 * 24 // s.
 export const USER_SESSION_COOKIE_MAX_AGE_REMOVED = -1 // s.
