7237732cf7cbbe8fc5b63b73ce2d9a563a83e38b
王子凌 <sling1994@live.com>
2023-04-18 16:27:40 +0800

    test(server): enhance the tests

--- server/tests/api/account.unit.ts
+++ server/tests/api/account.unit.ts
@@ -4,6 +4,7 @@ import _ from 'lodash'
 import fetch from 'node-fetch'
 import shared from '@sling/artus-web-shared'
 import { UserSession } from '@sling/artus-web-shared/types'
+import * as ws from 'ws'
 
 describe('Check server running correctly', () => {
   const url = require('url')
@@ -50,14 +51,13 @@ describe('Check server running correctly', () => {
       hostname = '127.0.0.1'
     }
 
-    const ws = require('ws')
-
     const websocket = new ws.WebSocket(
       (global.websocketUri = url.format({ hostname, port, protocol: 'ws' })),
       global.request
     )
-    const { p, resolve } = shared.utils.generateOperablePromise<typeof ws.WebSocket.Event>()
+    const { p, resolve, reject } = shared.utils.generateOperablePromise<typeof ws.WebSocket.Event>()
     websocket.onopen = resolve.bind(websocket)
+    websocket.onerror = reject.bind(websocket)
 
     await shared.utils.promiseTimeout(p, { timeout: 3000 })
 
@@ -74,6 +74,8 @@ describe<LocalTestContext>('Account certificates', () => {
   }
   const accountCertificateNewPwd = Buffer.from('1qaz!QAZ1', 'utf-8').toString('base64')
   let accountAlreadySignedUp = false
+  const websocketObserveMessages: string[] = []
+  let websocketClient: ws.WebSocket | undefined
 
   it<LocalTestContext>('Sign-up', async () => {
     const signUpUri = url.format(
@@ -97,7 +99,7 @@ describe<LocalTestContext>('Account certificates', () => {
     ).then((res) => {
       const setCookies = res.headers.get('set-cookie') ?? ''
       if (setCookies) {
-        _.set(global, 'request.headers.cookie', setCookies.split(';')[0]).filter(Boolean).join(';')
+        _.set(global, 'request.headers.Cookie', setCookies.split(';')[0] ?? '')
       }
 
       return res.json()
@@ -130,7 +132,14 @@ describe<LocalTestContext>('Account certificates', () => {
         },
         global.request
       )
-    ).then((res) => res.json())
+    ).then((res) => {
+      const setCookies = res.headers.get('set-cookie') ?? ''
+      if (setCookies) {
+        _.set(global, 'request.headers.Cookie', setCookies.split(';')[0] ?? '')
+      }
+
+      return res.json()
+    })
 
     assert(_.get(session, 'status') === 'SUCCESS')
 
@@ -213,12 +222,122 @@ describe<LocalTestContext>('Account certificates', () => {
         },
         global.request
       )
-    ).then((res) => res.json())
+    ).then((res) => {
+      const setCookies = res.headers.get('set-cookie') ?? ''
+      if (setCookies) {
+        _.set(global, 'request.headers.Cookie', setCookies.split(';')[0] ?? '')
+      }
+
+      return res.json()
+    })
 
     assert(_.get(result, 'status') === 'SUCCESS')
 
     const account = _.get(result, 'data.account')! as UserSession
     expect(account)
     assert(account.signedIn === true)
+  })
+
+  it<LocalTestContext>('Account-Observe', async () => {
+    const accountObserveUri = url.format(
+      _.merge(url.parse(global.websocketUri), {
+        pathname: '/ws/account/observe'
+      })
+    )
+
+    websocketClient = new ws.WebSocket(accountObserveUri, global.request)
+    const { p, resolve, reject } = shared.utils.generateOperablePromise<typeof ws.WebSocket.Event>()
+    websocketClient.onopen = resolve.bind(websocketClient)
+    websocketClient.onerror = resolve.bind(reject)
+
+    await shared.utils.promiseTimeout(p, { timeout: 3000 })
+
+    websocketClient!.addEventListener(
+      'message',
+      async function onMessage(event) {
+        websocketObserveMessages.push(event.data)
+      }.bind(websocketClient)
+    )
+  })
+
+  it<LocalTestContext>('Try-Sign-In-Again-After-Signed-In', async () => {
+    const signInUri = url.format(
+      _.merge(url.parse(global.httpUri), {
+        pathname: '/api/account/sign-in'
+      })
+    )
+
+    const result = await fetch(
+      signInUri,
+      _.merge(
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(_.pick(accountCertificate, ['email', 'password']))
+        },
+        global.request
+      )
+    ).then((res) => {
+      const setCookies = res.headers.get('set-cookie') ?? ''
+      if (setCookies) {
+        _.set(global, 'request.headers.Cookie', setCookies.split(';')[0] ?? '')
+      }
+
+      return res.json()
+    })
+
+    assert(_.get(result, 'status') === 'FAIL')
+    assert(_.get(result, 'code') === 'ERROR_SIGN_IN_ALREADY_SIGNED_IN')
+  })
+
+  it<LocalTestContext>('Try-Sign-In-Incognito', async () => {
+    const signInUri = url.format(
+      _.merge(url.parse(global.httpUri), {
+        pathname: '/api/account/sign-in'
+      })
+    )
+
+    // Another event.
+    websocketClient!.addEventListener(
+      'message',
+      async function onMessage(event) {
+        try {
+          const commandInfo = JSON.parse(event.data)
+          // The previous session will be evicted.
+          if (commandInfo.command === 'session-evict') {
+            await websocketClient!.close()
+          }
+        } catch (e) {}
+      }.bind(websocketClient)
+    )
+
+    // New sign in request.
+    // Will evict the previous signed-in session.
+    // Will manufacture a new session cookie key.
+    const result = await fetch(
+      signInUri,
+      _.merge({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(_.pick(accountCertificate, ['email', 'password']))
+      })
+    ).then((res) => {
+      const setCookies = res.headers.get('set-cookie') ?? ''
+      if (setCookies) {
+        _.set(global, 'request.headers.Cookie', setCookies.split(';')[0] ?? '')
+      }
+
+      return res.json()
+    })
+
+    await shared.utils.promiseDelay(100)
+    assert(_.get(result, 'status') === 'SUCCESS')
+    expect(websocketClient)
+    assert(websocketClient!.readyState === websocketClient!.CLOSED)
+    assert(_.last(websocketObserveMessages)!.includes('DISABLE_MULTIPLE_SIGNED_IN_SESSIONS'))
   })
 })
