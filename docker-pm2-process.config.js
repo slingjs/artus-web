f0d6f1e935f75ea2470e58d247f0406a24010c7b
王子凌 <sling1994@live.com>
2023-05-06 17:16:55 +0800

    chore: format the files

--- docker-pm2-process.config.js
+++ docker-pm2-process.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "artus-web",
-      script: "pnpm start",
-      exec_mode: "cluster",
+      name: 'artus-web',
+      script: 'pnpm start',
+      exec_mode: 'cluster',
       instances: 2,
-      max_memory_restart: "260M",
-      cwd: "/app",
-      ignore_watch: ["node_modules"],
+      max_memory_restart: '260M',
+      cwd: '/app',
+      ignore_watch: ['node_modules'],
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: 'production'
       }
     }
   ]
