acd13264ddceb4a5b562df958fdf676e19193b98
王子凌 <sling1994@live.com>
2023-05-06 18:57:04 +0800

    chore(shared): add some websocket related constants

--- packages/shared/constants/session.ts
+++ packages/shared/constants/session.ts
@@ -1,2 +1,4 @@
 export const USER_SESSION_KEY = 'artus-web.user.session'
 export const USER_CSRF_TOKEN_KEY = 'X-CSRF-Token'
+export const WEBSOCKET_SUB_PROTOCOL = 'artus-web.ws.protocol'
+export const WEBSOCKET_SUB_PROTOCOL_LINKER = '__'
